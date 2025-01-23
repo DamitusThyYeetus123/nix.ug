d4f0bc95f4da5e21ef964adf047d7b18e6ea73a1
DamitusThyYeetus123 <108782125+DamitusThyYeetus123@users.noreply.github.com>
2025-01-23 14:51:55 +1100

    Create default.nix

---
+++ lists/NUGs/snug/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = "Online"; # space separated keywords
+  name = "SNUG";
+  subtitle =  "No strict schedule";
+  tag = "Online";
+  target = "_blank"; # optional html <a> tag target attribute
+  url = "https://snugroup.org";
+}
